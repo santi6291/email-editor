c265441f0e2170736a33c4c7413ba1eb6ec7ec45
Santiago J Sosa <ssosa@santiagojsosa.com>
2015-02-24 11:35:12 -0500

    spacing

--- template.php
+++ template.php
@@ -2,6 +2,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require_once($_SERVER['DOCUMENT_ROOT'] . 'app/conf.php');
+
 require_once ($paths['data']['templates']['parts'] . 'head.html');
+
 require_once ($paths['data']['templates']['stored'] . $_GET['template'] . '/' . $_GET['version'] . '.html') ;
+
 require_once ($paths['data']['templates']['parts'] . '/footer.html');
